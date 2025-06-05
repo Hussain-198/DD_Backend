b6f065d60071af74bae35916180ae08d67648a0f
Zahid Hussain Zakir Hussain <zhussain@freshworks.com>
2025-06-05 18:49:32 +0530

    feat: added allow methods for cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    credentials: true
+    credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
