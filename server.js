1d129fec1f897a987483436e6a30565c3af776ef
Zahid Hussain Zakir Hussain <zhussain@freshworks.com>
2025-06-05 18:46:21 +0530

    feat: added home url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    res.send(200).json({ message: "welcome" })
+    res.status(200).json({ message: "welcome" })
 })
 
 const authRoutes = require("./routes/authRoutes");
@@ -29,8 +29,10 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected");
-        app.listen(process.env.PORT || 5000, () =>
-            console.log("Server running in port", process.env.PORT || 5000)
-        );
+
     })
     .catch((err) => console.error("MongoDB connection error: ", err));
+
+app.listen(process.env.PORT || 5000, () =>
+    console.log("Server running in port", process.env.PORT || 5000)
+);
